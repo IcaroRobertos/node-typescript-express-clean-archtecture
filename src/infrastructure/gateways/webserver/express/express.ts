5b8f816f0be0a2ef1b138d499f45a31bbf0128bc
icaro <oi@icaroroberto.com>
2021-07-18 18:33:38 -0300

    feat: webserver read routes

--- src/infrastructure/gateways/webserver/express/express.ts
+++ src/infrastructure/gateways/webserver/express/express.ts
@@ -6,7 +6,7 @@ import { routesDefinitions } from '@httpRoutes';
 import { httpRequestAdapter } from '@httpPort/adapters/http_request_adapter';
 
 const preRoutesMiddlewares = (app: Express) => {
-  app.use(cors);
+  app.use(cors());
   app.use(json());
   app.use(urlencoded({ extended: false }));
 };
@@ -16,8 +16,6 @@ const handleRoutes = (appRoutes: Router) => {
     routes.forEach(({ controller, method, path }) => {
       appRoutes[method](`${prefix}${path}`, async (req: Request, res: Response) => {
         const { body, httpStatus } = await controller(httpRequestAdapter(req));
-
-        console.log('{ body, httpStatus }', { body, httpStatus });
 
         return res.status(httpStatus).send(body);
       });
@@ -29,12 +27,10 @@ export const webServer = (appPort?: number) => {
   const app = express();
   const routes = express.Router();
 
-  routes.get('/b', (_req, res) => res.send(2));
-
   preRoutesMiddlewares(app);
   handleRoutes(routes);
 
-  console.log('app.routes', routes.stack);
+  app.use(routes);
 
   if (appPort) return app.listen(appPort, () => { console.log(`Webserver running on port ${appPort}`); });
 
--- src/infrastructure/ports/http/routes/root.ts
+++ src/infrastructure/ports/http/routes/root.ts
@@ -3,24 +3,9 @@ import { RouteDefinitionTypes } from '../typings/route.types';
 
 export const rootRoutes: RouteDefinitionTypes = {
   prefix: '',
-  routes: [
-    {
-      method: GET,
-      path: '/',
-      controller: async (_req) => {
-        console.log('AQUIIII');
-
-        return { body: true, httpStatus: 200 };
-      },
-    },
-    {
-      method: GET,
-      path: '/asd',
-      controller: async (_req) => {
-        console.log('AQUIIII');
-
-        return { body: true, httpStatus: 200 };
-      },
-    },
-  ],
+  routes: [{
+    method: GET,
+    path: '/',
+    controller: async (_req) => ({ body: true, httpStatus: 200 }),
+  }],
 };
